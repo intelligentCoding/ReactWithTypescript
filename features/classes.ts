37857708bd3dcb0541d9cb732ea41a55c4924f61
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-03-26 18:41:37 -0600

    classes with inheritence

--- features/classes.ts
+++ features/classes.ts
@@ -1,13 +1,35 @@
 class Vehicle {
-    drive() : void {
-        console.log("Driving Away")
-    }
-
-    honk() : void {
+    // color : string = 'red';
+    constructor( public color : string) {}
+    protected honk() : void {
         console.log("Beep")
     }
 }
 
-const vehicle  = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+const vehicle  = new Vehicle('orange');
+console.log(vehicle.color)
+
+class Cars  extends Vehicle{
+    constructor(public wheels : number, color : string){
+        super(color);
+    }
+    private drive() : void {
+        console.log("Not Driving away the car")
+    }
+
+    startDriving() : void {
+        this.drive();
+        this.honk();
+    }
+}
+
+// const vehicle  = new Vehicle();
+// vehicle.drive();
+// vehicle.honk();
+
+const cars = new Cars(4,'red');
+//drive() is over ridden
+cars.startDriving();
+// cars.honk();
+
+

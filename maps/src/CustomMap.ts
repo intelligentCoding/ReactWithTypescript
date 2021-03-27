df7f855d94d6effdef3f24b0aef7222acd103b2c
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-03-27 10:41:54 -0600

    company and user marker added

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,5 +1,6 @@
 import { User } from './User';
 import { Company } from './Company';
+import { company } from 'faker';
 
 export class CustomMap {
     private googleMap: google.maps.Map;
@@ -20,6 +21,15 @@ export class CustomMap {
             position: {
                 lat: user.location.lat,
                 lng: user.location.lng
+            }
+        })
+    }
+    addCompanyMarker (company : Company ) : void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: company.location.lat,
+                lng: company.location.lng
             }
         })
     }
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,13 +1,11 @@
 import { User } from './User';
-// import { Company } from './Company';
+import { Company } from './Company';
 import { CustomMap } from "./CustomMap";
-// const user = new User();
-// console.log(user)
 
-// const company = new Company();
-// console.log(company);
+const company = new Company();
 const user = new User();
 const customMap  = new CustomMap("map");
 
-customMap.addUserMarker(user)
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company)
 

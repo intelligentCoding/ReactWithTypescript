81025af3f47abede8d7246a9874433ce67b0c8b2
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-03-26 18:16:25 -0600

    interfaces

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,12 +1,18 @@
 interface Vehicle {
     name: string;
-    year: number;
+    // year: number;
+    year: Date;
     broken: boolean;
+    summary(): string;
 }
 const oldHonda = {
     name: 'civic',
-    year: 2000,
-    broken: true
+    // year: 2000,
+    year: new Date(),
+    broken: true,
+    summary() : string{
+        return `Name : ${this.name}`
+    }
 };
 
 // const printVehicle = (vehicle : { name: string; year: number;broken: boolean}) : void => {

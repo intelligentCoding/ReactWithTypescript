6d43236ddf919d3102b764f6306ec41f6227863d
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-03-26 22:16:28 -0600

    basic setup

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,7 @@
-console.log("This is Kash man");
+// import { User } from './User';
+// import { Company } from './Company';
+// const user = new User();
+// console.log(user)
+
+// const company = new Company();
+// console.log(company);

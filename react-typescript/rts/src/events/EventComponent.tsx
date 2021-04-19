909cf4c2a22f61f945b55de03effcc82d75b1591
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-04-19 08:10:04 -0600

    drag event

--- react-typescript/rts/src/events/EventComponent.tsx
+++ react-typescript/rts/src/events/EventComponent.tsx
@@ -1,10 +1,16 @@
+import React from "react";
+
 const EventComponent : React.FC= () => {
 
     const onChange = (event : React.ChangeEvent<HTMLInputElement>) => {
         console.log(event.target.value);
     }
+    const onDragStart = (event : React.DragEvent<HTMLDivElement>) =>{
+        console.log(event)
+    }
     return <div>
         <input onChange={onChange}/>
+        <div draggable onDragStart={onDragStart}>Drage Me</div>
     </div>
 }
 

e2d859c681042a9a2cfffcaefcdb0563499bd914
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-04-19 08:00:16 -0600

    events and props example

--- react-typescript/rts/src/index.tsx
+++ react-typescript/rts/src/index.tsx
@@ -1,14 +1,17 @@
 import ReactDOM from 'react-dom';
-
+import GuestList from './state/GuestList';
+import UserSearch from './state/UserSearch';
+import EventComponent from './events/EventComponent'
 
 const App = () => {
-    return <div>
-        <h1>Hi There</h1>
+  return (
+    <div>
+      {/* <GuestList /> */}
+      {/* <UserSearch/> */}
+      <EventComponent/>
+
     </div>
-}
+  );
+};
 
-
-ReactDOM.render(
-    <App/>,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
---
+++ react-typescript/rts/src/state/UserSearch.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+
+const users = [
+    {name: 'Kashif', age: 20},
+    {name: 'Anushree', age: 20},
+    {name: 'Ankit', age: 20},
+    {name: 'Santosh', age: 20},
+]
+const UserSearch : React.FC = () => {
+
+    const [name, setName] = useState('');
+    const [user, setUser] = useState<{name :string, age: number} | undefined>();
+
+    const onClick = () => {
+        const foundUser = users.find((user)=>{
+            return user.name === name;
+        })
+        setUser(foundUser)
+    }
+    return <div>User Search
+
+        <input value={name} onChange={(e) => setName(e.target.value)}/>
+        <button onClick={onClick}> Find User</button>
+        <div>
+            {user && user.name}
+            {user && user.age}
+        </div>
+    </div>
+}
+
+export default UserSearch;

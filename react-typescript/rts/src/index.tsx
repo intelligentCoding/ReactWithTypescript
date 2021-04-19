9bce221e3ec60165cc2e741560239f18d3512a52
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-04-19 08:41:49 -0600

    userserch class in index

--- react-typescript/rts/src/index.tsx
+++ react-typescript/rts/src/index.tsx
@@ -1,14 +1,20 @@
 import ReactDOM from 'react-dom';
 import GuestList from './state/GuestList';
-import UserSearch from './state/UserSearch';
+import UserSearch from './classes/UserSearch';
 import EventComponent from './events/EventComponent'
 
+const users = [
+    {name: 'Kashif', age: 20},
+    {name: 'Anushree', age: 20},
+    {name: 'Ankit', age: 20},
+    {name: 'Santosh', age: 20},
+]
 const App = () => {
   return (
     <div>
       {/* <GuestList /> */}
-      {/* <UserSearch/> */}
-      <EventComponent/>
+      <UserSearch users={users}/>
+      {/* <EventComponent/> */}
 
     </div>
   );

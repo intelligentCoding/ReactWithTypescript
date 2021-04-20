bb90b0aea8692f76955c955b7d202c22331bda8b
Kashif Mahmood <kaaf_kaaf@hotmail.com>
2021-04-20 08:28:31 -0600

    repositories reducer finished

--- react-typescript/redux-ts/src/state/reducers/repositoriesReducer.ts
+++ react-typescript/redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -1,25 +1,45 @@
 interface RepositoriesState {
-    loading: boolean;
-    error: string | null;
-    data: string[];
+  loading: boolean;
+  error: string | null;
+  data: string[];
 }
 
+interface SearchRepositoriesAction {
+  type: ActionType.SEARCH_REPOSITORIES;
+}
+interface SearchRepositoriesSuccessAction {
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+  payload: string[];
+}
+interface SearchRepositoriesSuccessErrorAction {
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
+  payload: string;
+}
 
-interface Action {
-    type: string;
-    payload?:any;
+type Action =
+  | SearchRepositoriesAction
+  | SearchRepositoriesSuccessAction
+  | SearchRepositoriesSuccessErrorAction;
+
+enum ActionType {
+  SEARCH_REPOSITORIES = "search_repositories",
+  SEARCH_REPOSITORIES_SUCCESS = "search_repositories_success",
+  SEARCH_REPOSITORIES_ERROR = "search_repositories_error",
 }
-const reducer = (state : RepositoriesState, action: Action) : RepositoriesState => {
-    switch (action.type) {
-        case 'search_repositories':
-            return { loading: true, error: null, data: []}
-        case 'search_repositories_success':
-            return {loading: false, error: null, data: action.payload}
-        case 'search_repositories_error':
-            return {loading: false, error: action.payload, data: []}    
-        default:
-            return state;
-    }
-}
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return { loading: true, error: null, data: [] };
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return { loading: false, error: null, data: action.payload };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
